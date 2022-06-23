e00241e352068a40bd447b204fd7bcac86fd0060
MahFLima <mariana.lima@uni9.edu.br>
2022-06-23 08:30:35 -0300

    .env

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,8 @@
+
+
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4oacm4j1jlk01xmhr0xgq0j/master',
+  uri: process.env.REACT_URI,
   cache: new InMemoryCache()
 })
